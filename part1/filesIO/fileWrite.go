01dec42b567b15f75a0887eb4e1c3c905450d944
Алексей <zotov.alexei.1@yandex.ru>
2021-02-22 21:27:32 +0300

    write to file function

--- part1/filesIO/fileWrite.go
+++ part1/filesIO/fileWrite.go
@@ -1,1 +1,33 @@
 package filesIO
+
+import (
+	"io"
+	"os"
+)
+
+func writeLines(file io.Writer, lines []string) error {
+	for _, line := range lines {
+		_, err := io.WriteString(file, line+"\n")
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func write(fileName string, outputLines []string) error {
+	var file *os.File
+
+	if fileName != "" {
+		// если был передан файл, пишем в него
+		file, err := os.Create(fileName)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+	} else {
+		file = os.Stdout
+	}
+
+	return writeLines(file, outputLines)
+}
