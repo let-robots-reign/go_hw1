8cfc04d24e545a976622ec4b0f611de5c7bc7c9d
Алексей <zotov.alexei.1@yandex.ru>
2021-02-23 11:52:42 +0300

    fixed -i, simplified uniq

--- part1/uniq/unique.go
+++ part1/uniq/unique.go
@@ -16,38 +16,52 @@ type Options struct {
 	IgnoredCharsNum  int
 }
 
-func FindUnique(lines []string, options Options) (result []string, error error) {
-	stringsOccurrences := make(map[string]int)
+type StringInfo struct {
+	original string // оригинальная строка до ToLower() и пропуска слов/символов
+	repeats  int    // количество повторений
+}
 
-	// TODO: fix -i, add -f and -c
+func NewStringInfo(original string) StringInfo {
+	return StringInfo{
+		original: original,
+		repeats:  1,
+	}
+}
+
+func FindUnique(lines []string, options Options) (result []string, error error) {
+	stringsOccurrences := make(map[string]*StringInfo)
+
+	// TODO: add -f and -c
 
 	for _, str := range lines {
+		original := str
+
 		if options.CaseInsensitive {
 			str = strings.ToLower(str)
 		}
 
-		stringsOccurrences[str]++
+		if _, exists := stringsOccurrences[str]; exists {
+			stringsOccurrences[str].repeats++
+		} else {
+			newKey := NewStringInfo(original)
+			stringsOccurrences[str] = &newKey
+		}
 	}
 
-	if options.Duplicate {
-		for key, value := range stringsOccurrences {
-			if value > 1 {
-				result = append(result, key)
+	for _, value := range stringsOccurrences {
+		switch {
+		case options.Duplicate:
+			if value.repeats > 1 {
+				result = append(result, value.original)
 			}
-		}
-	} else if options.Unique {
-		for key, value := range stringsOccurrences {
-			if value == 1 {
-				result = append(result, key)
+		case options.Unique:
+			if value.repeats == 1 {
+				result = append(result, value.original)
 			}
-		}
-	} else if options.Count {
-		for key, value := range stringsOccurrences {
-			result = append(result, strconv.Itoa(value)+" "+key)
-		}
-	} else {
-		for key, _ := range stringsOccurrences {
-			result = append(result, key)
+		case options.Count:
+			result = append(result, strconv.Itoa(value.repeats)+" "+value.original)
+		default:
+			result = append(result, value.original)
 		}
 	}
 
