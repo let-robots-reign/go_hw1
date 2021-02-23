b564e0d1b5728c2f238495681a786957f8fe45c8
Алексей <zotov.alexei.1@yandex.ru>
2021-02-23 13:44:57 +0300

    prepare for testing

---
+++ part1/test/test_unique.go
@@ -0,0 +1,39 @@
+package test
+
+import (
+	"github.com/stretchr/testify/require"
+	"go_hw1/part1/uniq"
+	"testing"
+)
+
+func TestWithoutOptions(t *testing.T) {
+
+}
+
+func TestDuplicates(t *testing.T) {
+
+}
+
+func TestUnique(t *testing.T) {
+
+}
+
+func TestCount(t *testing.T) {
+
+}
+
+func TestCaseInsensitive(t *testing.T) {
+
+}
+
+func TestIgnoreFields(t *testing.T) {
+
+}
+
+func TestIgnoreChars(t *testing.T) {
+
+}
+
+func TestErrors(t *testing.T) {
+
+}
